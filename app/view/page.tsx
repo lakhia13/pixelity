64e521e0a9fc7d30601c1017974ac32d25651a1a
pranshav <pranshav.lakhia@gmail.com>
2025-03-30 01:00:25 -0400

    Added views page setup

---
+++ app/view/page.tsx
@@ -0,0 +1,84 @@
+//
+// Filename: page.tsx
+// Route: /
+// Created: 3/29/25
+//
+
+"use client";
+
+import { Button } from 'pixel-retroui';
+import { useState, JSX } from 'react';
+import '@/lib/pixel-retroui-setup.js';
+
+export default function Home() {
+    const [selectedButton, setSelectedButton] = useState('List');
+    const [view, setView] = useState('List');
+
+	const onClick = (buttonName: string) => {
+		setSelectedButton(buttonName);
+        setView(buttonName);
+		console.log(`Clicked ${buttonName}`);
+	};
+
+    const ViewMode = ():JSX.Element | null =>{
+        if (view === 'Albums') {
+            // Render the albums view
+            return (
+                <div className="pl-8 py-3">
+                    <h1 className="text-2xl">Albums View</h1>
+                    {/* Add your albums view content here */}
+                </div>
+            );
+        } else if (view === 'List') {
+            // Render the all view
+            return (
+                <div className="pl-8 py-3">
+                    <h1 className="text-2xl">List View</h1>
+                    {/* Add your all view content here */}
+                </div>
+            );
+        } else if (view === 'Grid') {
+            // Render the all view
+            return (
+                <div className="pl-8 py-3">
+                    <h1 className="text-2xl">Grid View</h1>
+                    {/* Add your all view content here */}
+                </div>
+            );
+        } else {
+            // Render a default view or return null
+            return null;
+        }
+    };
+	return (
+		<div>
+			<main className='relative flex'>
+                <ViewMode/>
+				<div className="absolute right-0 flex justify-end items-start my-4">
+					<Button onClick={() => onClick('Albums')}className={`${selectedButton === 'Albums' ? 'bg-gray-300' : ''}`}
+                        style={{
+							backgroundColor: selectedButton === 'Albums' ? '#e5e7eb' : 'transparent',
+							color: selectedButton === 'Albums' ? '#1f2937' : '#6b7280',
+						}}>
+						Albums
+					</Button>
+                    <Button onClick={() => onClick('Grid')} className={`px-2 ${selectedButton === 'Grid' ? 'bg-gray-300' : ''}`}
+                        style={{
+							backgroundColor: selectedButton === 'Grid' ? '#e5e7eb' : 'transparent',
+							color: selectedButton === 'Grid' ? '#1f2937' : '#6b7280',
+						}}>
+						Grid
+					</Button>
+                    <Button onClick={() => onClick('List')} className={`px-4 ${selectedButton === 'List' ? 'bg-gray-300' : ''}`}
+                        style={{
+							backgroundColor: selectedButton === 'List' ? '#e5e7eb' : 'transparent',
+							color: selectedButton === 'List' ? '#1f2937' : '#6b7280',
+						}}>
+						List
+					</Button>
+				</div>
+                
+			</main>
+		</div>
+	);
+}

651a00b87469af04134e3a06324b06556c200fb0
Ryan Smith <rysmith2113@gmail.com>
2025-03-30 01:00:30 -0400

    Resize the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@
 // Created: 3/29/25
 //
 
-"use client";
+"use client"
 
 import { Button, Card } from "pixel-retroui";
 import { JSX } from "react";
@@ -18,8 +18,11 @@ export default function NavBar(): JSX.Element {
 				<Image src="/logo.png" alt="Pixelity Logo" width={75} height={75}/>
 			</Card>
 			
-			<div className="rounded-lg bg-white border-0 border-b-2 border-solid border-b-red-600 text-black w-full flex justify-center items-center">
-				<p className="text-7xl text-center">Pixelity</p>
+			<div className={`
+				rounded-lg bg-white border-b-2 border-solid border-b-red-600
+				text-black w-full flex justify-center items-center p-4
+			`}>
+				<p className="text-5xl text-center">Pixelity</p>
 			</div>
 
 			<Button onClick={void(0)}> <MdAccountCircle size={65}/> </Button>

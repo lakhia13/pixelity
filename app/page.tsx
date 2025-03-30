e9db36ccd8af65f20dcebb87a8a03e16c3cc32f5
Ryan Smith <rysmith2113@gmail.com>
2025-03-30 03:08:47 -0400

    Reorganize the home page

--- app/page.tsx
+++ app/page.tsx
@@ -21,50 +21,46 @@ export default function Home() {
 	const togglePopup = () => setIsOpen((prev) => !prev);
 	
 	return (
-		<div className="my-auto">
-			<main>
-				<div className="flex justify-center items-center w-full">
-					<div className="justify-start items-start my-[300px] flex flex-col w-1/3">
-						<Bubble
-							className="flex justify-center items-center"
-							direction="left"
-							onClick={ () => {
-								console.log("Clicked");
-							}}
-						>
-							Your Images, Under Your Control
-						</Bubble>
-						<Image src="/mascot_no_bg.png" alt="Pixelity Logo" width={300} height={300}/>
-					</div>
-					
-					<div className="justify-end items-end my-[300px] w-1/4 flex flex-col">
-						<Accordion>
-							<AccordionItem value="item-1">
-								<AccordionTrigger>Why use Pixelity</AccordionTrigger>
-								
-								<AccordionContent>
-									If you have an old android phone lying around, with no use than gathering dust. And if you 
-									don&apos;t want to hand over your photos to Big Data, then you have come to the right place.
-								</AccordionContent>
-							</AccordionItem>
+		<div className="flex justify-center items-center mt-[100px]">
+			<main className="flex justify-center items-center">
+				<div className="flex flex-col items-center">
+					<div className="flex justify-center items-center">
+						<div className="flex flex-col w-1/3">
+							<Bubble direction="left"> Your Images, Under Your Control </Bubble>
+							<Image src="/mascot_no_bg.png" alt="Pixelity Logo" width={300} height={300}/>
+						</div>
+						
+						<div className="w-1/4 flex flex-col">
+							<Accordion>
+								<AccordionItem value="item-1">
+									<AccordionTrigger>Why use Pixelity</AccordionTrigger>
+									
+									<AccordionContent>
+										If you have an old android phone lying around, with no use than gathering dust. And if you 
+										don&apos;t want to hand over your photos to Big Data, then you have come to the right place.
+									</AccordionContent>
+								</AccordionItem>
 
-							<AccordionItem value="item-2">
-								<AccordionTrigger>How does it Work</AccordionTrigger>
+								<AccordionItem value="item-2">
+									<AccordionTrigger>How does it Work</AccordionTrigger>
 
-								<AccordionContent>
-									Our android app interfaces with our web login. You can use your old phone as your personal,
-									private cloud server. Your photos will be stores on your device, accessible to you only,
-									from anywhere you want! 
-								</AccordionContent>
-							</AccordionItem>
-						</Accordion>
+									<AccordionContent>
+										Our android app interfaces with our web login. You can use your old phone as your personal,
+										private cloud server. Your photos will be stores on your device, accessible to you only,
+										from anywhere you want! 
+									</AccordionContent>
+								</AccordionItem>
+							</Accordion>
+						</div>
 					</div>
 
-					<SessionProvider>
-						<AuthPopup isOpen={isOpen} onClose={togglePopup}/>
-					</SessionProvider>
-					
-					<Button onClick={togglePopup}>Open Popup</Button>
+					<div className="flex justify-center items-center mt-4">
+						<SessionProvider>
+							<AuthPopup isOpen={isOpen} onClose={togglePopup}/>
+						</SessionProvider>
+						
+						<Button className="w-[300px]" onClick={togglePopup}>Get Started</Button>
+					</div>
 				</div>
 			</main>
 		</div>
